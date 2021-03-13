556a27bf0794be46dbce5a06070fe14fd383284a
Abuenameh <abuenameh@gmail.com>
2021-03-13 01:39:28 -0400

    Update duck.sh

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -2,8 +2,11 @@
 
 . /app/duck.conf
 IP=$(wget -qO- "http://myexternalip.com/raw")
+echo $IP
 IPV6=$(wget -q --output-document - http://checkipv6.dyndns.com/ | grep -o "[0-9a-f\:]\{8,\}")
+echo $IPv6
 RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip=$IP&ipv6=$IPV6"`
+echo $RESPONSE
 if [ "$RESPONSE" = "OK" ]; then
 echo "Your IP was updated at "$(date)
 else
